4e25728fa6e225cd29903cfee8f81a392e3f4615
akflds <andrewkirsfelds@gmail.com>
2022-05-16 11:23:55 +0100

    implement GET on /api/topics with tests

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const { getTopics } = require("./controllers/topics.controller");
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/topics", getTopics);
+
+app.use("/*", (req, res, next) => {
+  res.status(404).send({ msg: "Route not found." });
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send({ msg: "Internal server error." });
+});
 
 module.exports = app;

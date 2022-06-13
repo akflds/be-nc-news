95288be30cbe5e42f9579eda441cc81ac842d185
akflds <andrewkirsfelds@gmail.com>
2022-06-13 10:03:57 +0100

    use cors

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const apiRouter = require("./routes/api-router.js");
 const app = express();
+const cors = require("cors");
+app.use(cors());
 app.use(express.json());
 
 app.use("/", express.static("static"));
